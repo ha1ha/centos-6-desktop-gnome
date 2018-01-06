e6e117a79babba5f120be97b69b1fa6710b4bd95
root <root@localhost.localdomain>
2018-01-06 13:38:26 +0100

    Correction de l'affichage.

--- postinstall.sh
+++ postinstall.sh
@@ -58,22 +58,22 @@ sleep $DELAY
 
 # Arrêt de PackageKit qui entre en conflit avec Yum
 if ps aux | grep packagekitd | grep -v grep 2>&1 > /dev/null ; then
-  echo "::"
   echo -e ":: Arrêt de PackageKit... \c"
   killall -9 packagekitd >> $LOG 2>&1
   echo -e "[${VERT}OK${GRIS}] \c"
   sleep $DELAY
   echo
+  echo "::"
 fi
 
 # Suppression de PackageKit
 if rpm -q PackageKit > /dev/null ; then
-  echo "::"
   echo -e ":: Suppression de PackageKit... \c"
   yum -y remove PackageKit >> $LOG 2>&1
   echo -e "[${VERT}OK${GRIS}] \c"
   sleep $DELAY
   echo
+  echo "::"
 fi
 
 # Mise à jour initiale
